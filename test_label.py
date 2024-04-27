2c7915d8e496e98e86777de8d2e07709cbbdaa23
Shejal Shankar <60665698+ShejalShankar@users.noreply.github.com>
2024-04-26 20:21:27 -0400

    Add files via upload

---
+++ test_label.py
@@ -0,0 +1,56 @@
+import cv2
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import pickle as pkl
+
+path = "Dataset1/TUSimple/"
+#files = ['label_data_0313.json','label_data_0531.json','label_data_0601.json']
+test_samples = []
+json_gt = [json.loads(line) for line in open('Dataset1/TUSimple/test_set/test_label_new.json')]
+
+for i in range(len(json_gt)) :
+        
+        print(i)
+        gt = json_gt[i]
+        gt_lanes = gt['lanes']
+        y_samples = gt['h_samples']
+        raw_file = gt['raw_file']
+        img = cv2.imread(path+raw_file)
+        gt_lanes_vis = [[(x, y) for (x, y) in zip(lane, y_samples)
+                    if x >= 0] for lane in gt_lanes]
+        img_vis = img.copy()
+        for lane in gt_lanes_vis:
+                img_poly = cv2.polylines(img_vis, np.int32([lane]), isClosed= False,
+                    color=(0,255,0), thickness=5)
+        #cv2.imshow('',img_vis)
+        #cv2.imwrite("images"+str(i)+".png",img_vis)
+        #cv2.waitKey(0)
+        #cv2.destroyAllWindows()
+        mask = np.zeros_like(img)
+        colors = [[255,0,0],[0,255,0],[0,0,255],[0,255,255],[255,255,255]]
+        for i in range(len(gt_lanes_vis)):
+            img_mask = cv2.polylines(mask,np.int32([gt_lanes_vis[i]]),isClosed=False,color=colors[i],thickness=5)
+        '''cv2.imshow('',img_mask)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    '''
+        label = np.zeros((720,1280),dtype = np.uint8)
+        for i in range(len(colors)):
+            label[np.where((mask == colors[i]).all(axis = 2))] = 1
+        final_image = cv2.resize(img,(480,480))
+        #print(final_image.shape)
+        f_label = cv2.resize(label,(480,480))
+        final_label = f_label.reshape(480,480,1)
+        
+        sample = {
+            "image": final_image,
+            "label": final_label
+        }
+
+        test_samples.append(sample)
+
+
+with open(path + 'testing_sample.pkl', 'wb') as f:
+      pkl.dump(test_samples, f)
